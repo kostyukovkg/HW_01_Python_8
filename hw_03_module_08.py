d0428a45a2b229bca96b38526a17ad7d4eeb541b
Kostya <kostyukovkg@yahoo.com>
2024-10-25 15:40:01 +0300

    Second solution guess_function_2 is provided. README file was updated.

--- hw_03_module_08.py
+++ hw_03_module_08.py
@@ -37,6 +37,43 @@ def guess_function(number:int=1) -> int:
             break
     return count
 
+def guess_function_2(number:int=1) -> int:
+    """Takes a given number and tries to guess it with a given algorithm
+
+    Args:
+        number (int, optional): hidden number. Defaults to 1.
+
+    Returns:
+        int: number of tries
+    """
+    # first random prediction 
+    predict = 50 #np.random.randint(1, 100)
+
+    # number of tries
+    count = 0
+    # we save unsuccessful attempts to restate the borders later
+    # first stated highest and lowest borders
+    predict_h = 100
+    predict_l = 1
+    while True:
+        # counter
+        count+=1
+        # if first prediction is more than a number, 
+        # remember the prediction as a high bound and make a new guess given new borders
+        if predict > number:
+            predict_h = predict
+            predict = int((predict_h + predict_l) / 2)
+            continue
+        # if first prediction is less than a number, 
+        # remember the prediction as a low bound and make a new guess given new borders
+        elif predict < number:
+            predict_l = predict
+            predict = int((predict_h + predict_l)/2)
+            continue
+        else:
+            break
+    return count
+
 def guess_game(guess_function) -> int:
     """Generates a random array of integers and calculates average number of tries to guess
 
@@ -50,9 +87,9 @@ def guess_game(guess_function) -> int:
     # make a list to fill with random 1000 numbers
     count_ls = []
     # fix the seed
-    np.random.seed(1) 
+    # np.random.seed(1) 
     # random array statement
-    random_array = np.random.randint(1, 100, size=1000)
+    random_array = np.random.randint(1, 100, 1000)
     
     # add a number of tries required to guess a given 'number'
     for number in random_array:
@@ -64,4 +101,5 @@ def guess_game(guess_function) -> int:
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return score
 
-guess_game(guess_function)
+
+guess_game(guess_function_2)
